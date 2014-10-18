f576d544e4cdedbc19b228b13b383db914919853
Wendy Brannon <wbsolutionsllc2@gmail.com>
2014-10-17 19:32:11 -0600

    second try

--- app/controllers/prices_controller.rb
+++ app/controllers/prices_controller.rb
@@ -9,18 +9,15 @@ class PricesController < ApplicationController
   end
 
   def best_price
-    @price = Price.new(price_params)
-    if @price != nil
-    final_sale_price = params[:sale_price].to_f - (params[:sale_price].to_f * 0.10)
-    final_regular_price = params[:regular_price].to_f * (1 - params[:employee_discount].to_f)
+    if params[:sale_price] == nil || params[:sale_price] == nil || params[:employee_discount] == nil
+      final_sale_price = params[:sale_price].to_f - (params[:sale_price].to_f * 0.10)
+      final_regular_price = params[:regular_price].to_f * (1 - params[:employee_discount].to_f)
     if final_sale_price <= final_regular_price
       result_notice = "Go with sale price with 10% discount! $#{sprintf('%.2f', final_sale_price)}"
     else
       result_notice =  "Go with regular price with employee discount! $#{sprintf('%.2f', final_regular_price)}"
     end
-    redirect_to '/', notice: result_notice
-    else
-    redirect_to '/'
+      redirect_to '/', notice: result_notice
     end
 
   def result
